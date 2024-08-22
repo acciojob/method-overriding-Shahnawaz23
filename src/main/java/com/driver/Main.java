f5551be0ae2e3974bfb3be98e72ded865dd8b509
Shahnawaz <sa.akhtar23@gmail.com>
2024-08-22 17:32:03 +0530

    added super keyword

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
-  public static void main(String args[]) {
+  public static void main(String[] args) {
       B obj = new B();
       System.out.println(obj.meth());
   }
@@ -15,6 +15,7 @@ class A {
 
 class B extends A {
     public String meth() {
-        return "Method is overridden in Extendend class B";
+        System.out.println(super.meth());
+        return "Method is overridden in extended class B";
     }
 }
